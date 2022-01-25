32e811cde50d52a896f253fa7ed13d46625a5d4c
GLBryan <garrettleebryan@gmail.com>
2022-01-25 14:19:10 -0500

    rough resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,27 @@
 const { Book, User } = require('../models');
+import '../controllers/user-controller'
+import { createUser, deleteBook, getSingleUser, login } from '../controllers/user-controller';
 
 const resolvers = {
   Query: {
-
+    me: async () => {
+      return (getSingleUser())
+    }
   },
 
   Mutation: {
+    login: async (parent, { email, password }) => {
+      return login(email, password)
+    },
+    addUser: async (parent, { username, email, password }) => {
+      return createUser(username, email, password)
+    },
+    saveBook: async () => {
 
+    },
+    removeBook: async (parent, { _id }) => {
+      return deleteBook(_id)
+    },
   }
 };
 
